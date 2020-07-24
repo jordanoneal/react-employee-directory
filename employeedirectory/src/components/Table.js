fb660bd6a5ef7febacf08cbe5e1d07e3fde0e450
jordan o'neal <joneal6694@gmail.com>
2020-07-24 12:17:54 -0600

    basic table

--- employeedirectory/src/components/Table.js
+++ employeedirectory/src/components/Table.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import TableHead from "./TableHead";
 import TableRow from "./TableRow";
 import API from "../utils/API";
-import Search from "./Search";
 
 const Table = () => {
   const [users, setUsers] = React.useState([]);
@@ -15,14 +14,12 @@ const Table = () => {
 
   useEffect(() => console.log(users), [users]);
 
-  sortUsers = (users) => {
-    users.sort();
-  }
+
 
   return (
     <table>
       <thead>
-        <TableHead onClick={() => sortUsers()}/>
+        <TableHead />
       </thead>
       <tbody>
         {users.map((user) => (
